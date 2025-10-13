3b521a4cdb1390b9f419c5e4faa60eb968cd8e31
eduardoamdev <eduardoamdev@gmail.com>
2025-10-13 23:27:18 +0400

    Navbar folder renamed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { NavigationBar } from "./ui/Navbar/NavigationBar";
+import { NavigationBar } from "./ui/NavigationBar/NavigationBar";
 import "./globals.css";
 
 const geistSans = Geist({
