1501457ae49dad34b48dfb496a3d343edcaef0fc
eduardoamdev <eduardoamdev@gmail.com>
2025-10-13 22:40:32 +0400

    Booking page implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,7 @@
-"use client";
-
-import { useEffect } from "react";
-import { getCalApi } from "@calcom/embed-react";
-import InlineWidget from "@calcom/embed-react";
-
 export default function Home() {
-  useEffect(() => {
-    (async () => {
-      const cal = await getCalApi({ namespace: "booking" });
-      cal("ui", {
-        theme: "light",
-        layout: "month_view",
-        hideEventTypeDetails: false,
-      });
-    })();
-  }, []);
-
   return (
-    <main className="min-h-screen bg-white flex items-start justify-center p-6">
-      <InlineWidget
-        calLink="eduardo-alvarez-2hzm31"
-        namespace="booking"
-        config={{ layout: "month_view", theme: "light" }}
-        style={{ width: "100%", height: "900px" }}
-      />
+    <main className="min-h-screen bg-black flex items-start justify-center p-6">
+      Welcome to Amdev Academy
     </main>
   );
 }
